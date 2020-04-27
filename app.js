bb9e3481da9241d5b94e5b13bc01c27ca044441e
wsxjames <wsxjames@163.com>
2020-04-27 19:56:09 -0400

    new start

--- app.js
+++ app.js
@@ -6,6 +6,33 @@ app.get('/', (req, res) => {
   res.send('Hello!')
 });
 
+const config = {
+    user: 'james',
+    password: 'wushixinDB!',
+    server: 'uni-app-server.database.windows.net', 
+    database: 'S1G8UniAppSys' 
+};
+
+app.get('/schools', (req, res)=> {
+    sql.connect(config, (err)=> {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select SID, Name from School', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a responses
+            res.send(recordset);
+            
+        });
+    });
+});
+
 app.listen(port, ()=>{
   console.log('API listening on port='+port);
 });
