11b38d54c048ba641718f42055fb72a18a619622
wsxjames <wsxjames@163.com>
2020-04-27 22:34:00 -0400

    auth issue

--- app.js
+++ app.js
@@ -5,19 +5,31 @@ let hbs=require('express-handlebars')
 const sql = require("mssql");
 
 
+// const config = {
+//     user: 'james',
+//     password: 'wushixinDB!',
+//     server: 'uni-app-server.database.windows.net', 
+//     database: 'S1G8UniAppSys' 
+// };
+
 const config = {
-    user: 'james',
-    password: 'wushixinDB!',
-    server: 'uni-app-server.database.windows.net', 
-    database: 'S1G8UniAppSys' 
+  user: 'wus4',
+  password: 'wushixinJames34',
+  server: 'golem.csse.rose-hulman.edu', 
+  database: '_S1G8UniAppSys' 
 };
 
-// const config = {
-//   user: 'wus4',
-//   password: 'wushixinJames34',
-//   server: 'golem.csse.rose-hulman.edu', 
-//   database: '_S1G8UniAppSys' 
-// };
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 app.engine('handlebars',hbs({defaultLayout:'main'}))
 app.set('view engine','handlebars')
