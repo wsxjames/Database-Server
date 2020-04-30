c2f7c2fad8604e28d2239e0ec38987f327bbbbd1
wsxjames <wsxjames@163.com>
2020-04-30 15:04:25 -0400

    finished for friday

--- app.js
+++ app.js
@@ -37,24 +37,64 @@ app.get('/',(req,res)=>{
 //   res.render('profile')
 // })
 
+  // let query="@RC =exec [dbo].[ReadUniversities]"
+  // if (req.query.gender!="") query+="@Gender='"+req.query.gender+"'";
+  // if (req.query.isFirstApplicant!="") query+=",@IsFirstApplicant="+req.query.isFirstApplicant;
+  // if (req.query.gradYear!="") query+=",@GradYear="+req.query.gradYear;
+  // if (req.query.GPA!="") query+=",@GPA="+req.query.GPA;
+  // if (req.query.highSchool!="") query+=",@HighSchool='"+req.query.highSchool+"'";
+  // if (req.query.stdTestType!="") query+=",@StdTestType='"+req.query.stdTestType+"'";
+  // if (req.query.stdTestScore!="") query+=",@StdTestScore="+req.query.stdTestScore;
+  // if (req.query.testLocation!="") query+=",@TestLocation='"+req.query.testLocation+"'";
+  // if (req.query.testCity!="") query+=",@TestCity='"+req.query.testCity+"'";
+  // if (req.query.activityType!="") query+=",@ActivityType='"+req.query.activityType+"'";
+  // if (req.query.compRank!="") query+=",@CompRank='"+req.query.compRank+"'";
+
+  // console.log(query)
+
 app.get('/schools', (req, res)=> {
     sql.connect(config, (err)=> {
     
         if (err) console.log(err);
-
-        // create Request object
         var request = new sql.Request();
            
-        // query to the database and get the records
-        request.query('select SID, Name from School', function (err, recordset) {
-            
+        // request.query(query, function (err, recordset) {
+        request.input('GPA',sql.Decimal,req.query.GPA)
+        request.input('Gender',sql.NVarChar,req.query.gender)
+        request.input('IsFirstApplicant',sql.Bit,req.query.isFirstApplicant)
+        request.input('GradYear',sql.Int,req.query.GPA)
+        request.input('HighSchool',sql.NVarChar,req.query.highSchool)
+        request.input('StdTestType',sql.NVarChar,req.query.stdTestType)
+        request.input('StdTestScore',sql.Int,req.query.stdTestScore)
+        request.input('TestLocation',sql.NVarChar,req.query.testLocation)
+        request.input('TestCity',sql.NVarChar,req.query.testCity)
+        request.input('ActivityType',sql.NVarChar,req.query.activityType)
+        request.input('CompRank',sql.NVarChar,req.query.compRank)
+        // request.input()
+        request.execute("[dbo].[ReadUniversities]", function (err, recordset) {
             if (err) console.log(err)
 
             // send records as a responses
             res.send(recordset);
             
         });
+       
     });
+});
+
+app.get('/students', (req, res)=> {
+  sql.connect(config, (err)=> {
+  
+      if (err) console.log(err);
+      var request = new sql.Request();
+      request.input('University',sql.NVarChar,req.query.university)
+      request.execute("[dbo].[ReadStudentByUniversitySec]", function (err, recordset) {
+          if (err) console.log(err)
+
+          res.send(recordset);
+      });
+     
+  });
 });
 
 //app.listen(port, ()=>{
