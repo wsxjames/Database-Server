57e59826256d6c70bdd2d52b74d6fccc0c5b32e3
wsxjames <wsxjames@163.com>
2020-05-08 06:50:45 -0400

    diff cor

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var cors = require('cors');
 var corsOptions = {
     // origin: 'http://localhost:8080',
     // origin: 'http://wu-uni-app.s3-website.us-east-2.amazonaws.com',
-    origin: 'http://uni-app-client.csse.rose-hulman.edu/',
+    origin: 'http://uni-app-client.csse.rose-hulman.edu',
     credentials: true };
 
 app.use(cors(corsOptions));
