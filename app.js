bac0293fe50d0d470c9428e074957c7aabf6a61e
wsxjames <wsxjames@163.com>
2020-05-22 00:28:47 -0400

    final

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const session=require("express-session")
 bodyParser = require('body-parser').json();
 var cors = require('cors');
 var corsOptions = {
-    origin: 'http://localhost:8080',
+    // origin: 'http://localhost:8080',
     // origin: 'http://wu-uni-app.s3-website.us-east-2.amazonaws.com',
-    // origin: 'http://uni-app-client.csse.rose-hulman.edu',
+    origin: 'http://uni-app-client.csse.rose-hulman.edu',
     credentials: true };
 
 app.use(cors(corsOptions));
